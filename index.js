2ac5eb80516b9e7de84ee14389969e9cdf027f2b
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-10-24 15:09:34 +0500

    Changes in search element

--- index.js
+++ index.js
@@ -98,14 +98,22 @@ function deleteElement(deletePosition) {
 
 /* Search Array Elements */
 let data1 = [1, 3, 6, 8, 9, 3, 1, 1, 1];
+let index1;
 let index = [];
+let index2 = 0;
 
 function searchElement(searchValue) {
     for (i = 0; i <= data1.length - 1; i++) {
         if (data1[i] === searchValue) {
-            // index = i;
-            index.push(i);
-            // break; /* --> Agar array mai duplicate data na hu tab break use kro element search krna k baad aga check nhi kra ga or console loop k bhr use hoga */
+
+            /* This for Add in variable index1*/
+            // index1 = i;  
+
+           /* This for array index*/
+            index[index2] = i;
+            index2++;
+            
+            // break; /* --> Agar array mai duplicate data na hu tab break use kro element search krna k baad aga check nhi kra ga or console loop k bhr use hoga, duplicate data mai break use na krna par last element/index return hoga output mai*/
         }
     }
     console.log(index);
