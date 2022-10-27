498316ae51c8b93d94233a5144a54956d048e629
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-10-28 00:34:08 +0500

    Add Even value code

--- index.js
+++ index.js
@@ -337,11 +337,11 @@ student.abc();  /* --> Static Method Call. Call with class name not with class o
 class one {
     constructor(name) {
         this.name = name;
-        console.log("Constructor one Called");
+        // console.log("Constructor one Called");
     }
 
     info() {
-        console.log(`In class one my name is ${this.name}`);
+        // console.log(`In class one my name is ${this.name}`);
     }
 }
 
@@ -352,10 +352,25 @@ class two extends one {  /* --> class two inherit with one class, extends keywor
     // }
 
     info() {
-        console.log(`In class two my name is ${this.name}`);
+        // console.log(`In class two my name is ${this.name}`);
         super.info();  /* Call class one info() method */
     }
 }
 
 let a = new two("Ahmed");
 a.info();
+
+
+/* Find Even Values from Array, Make Double of every even value, Find Array Length Manually */
+let value = [2,3,7,6,8,16];
+let even = [];
+let count = 0;
+
+for(i = 0; i < value.length; i++) {
+    if(value[i] % 2 === 0) {  /* --> For Find Even Value */
+        even.push(value[i] * 2);  /* Put even values in even array and Make Double of every even value with multiply by 2 */
+        count++;  /* Find Length of even array */
+    }
+}
+// console.log(even);
+// console.log(count);
