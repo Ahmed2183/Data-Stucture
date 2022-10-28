55e1c5f9cf568cb2b47e41d37d0e37fc10ea9ec9
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-10-28 16:13:19 +0500

    Counting od duplicate elements

--- index.js
+++ index.js
@@ -362,15 +362,25 @@ a.info();
 
 
 /* Find Even Values from Array, Make Double of every even value, Find Array Length Manually */
-let value = [2,3,7,6,8,16];
+let value = [2, 3, 7, 6, 8, 16];
 let even = [];
 let count = 0;
 
-for(i = 0; i < value.length; i++) {
-    if(value[i] % 2 === 0) {  /* --> For Find Even Value */
+for (i = 0; i < value.length; i++) {
+    if (value[i] % 2 === 0) {  /* --> For Find Even Value */
         even.push(value[i] * 2);  /* Put even values in even array and Make Double of every even value with multiply by 2 */
         count++;  /* Find Length of even array */
     }
 }
 // console.log(even);
 // console.log(count);
+
+
+/* Counting of duplicate elements */
+let array = [1, 3, 5, 6, 3, 1, 1, 1, 5, 5];
+var counts = {};
+
+for (var i = 0; i < array.length; i++) {
+    counts[array[i]] = (counts[array[i]] + 1) || 1;  /* --> If element not exist then add 1 else + 1 */
+}
+console.log(counts);
