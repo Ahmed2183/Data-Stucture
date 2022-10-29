5410fc85554f89155e3c71a9fd4346a788f973cd
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-10-29 16:24:41 +0500

    Queue(FIFO) Example

--- index.js
+++ index.js
@@ -502,3 +502,39 @@ obj.reverseString(str1);
 
 obj.display();
 
+
+/* Queue(FIFO) Example */
+//enqueue(push) and dequeue(pop) are operation of Queue
+let queue = [];
+let queueLength = queue.length;
+let maxVal = 5;
+
+function pushQ(value) {  /* Add element in array */
+    if (queueLength >= maxValue) {
+        console.log("Queue is full you not enter:", value)
+    }
+    else {
+        queue[queueLength] = value;
+        queueLength++;
+    }
+}
+
+// pushQ(50); pushQ(40); pushQ(30); pushQ(20); pushQ(10);
+
+function popQ() {  /* Remove element from array */
+    if (queueLength > 0) {
+        for (i = 0; i < queue.length; i++) {
+            queue[i] = queue[i + 1];
+        }
+        queueLength--;
+        queue.length = queueLength;
+    }
+    else {
+        console.log("Queue is already empty");
+    }
+}
+
+// popQ(); popQ(); popQ(); popQ(); popQ();
+
+// console.log(queue);
+
