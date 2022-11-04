f0645ea8163f20d5df4f52746f3f9b71139c0762
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-11-04 08:36:09 +0500

    Change comment

--- Data Stucture/Counting of Duplicate Elements.js
+++ Data Stucture/Counting of Duplicate Elements.js
@@ -3,6 +3,6 @@ let array = [1, 3, 5, 6, 3, 1, 1, 1, 5, 5];
 var counts = {};
 
 for (var i = 0; i < array.length; i++) {
-    counts[array[i]] = (counts[array[i]] || 0) + 1;  /* --> If (counts[array[i]] || 0) means counts[array[i]] defined ha ya zero ha tu plus 1 krday gi  */
+    counts[array[i]] = (counts[array[i]] || 0) + 1;  /* --> If (counts[array[i]] || 0) means counts[array[i]] defined(agar phela sa object mai) ha ya zero ha tu plus 1 krday gi  */
 }
 console.log(counts);
