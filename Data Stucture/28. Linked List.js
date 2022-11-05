cb5e1e1b0869b295f8cbeb06f705f3fd7e8c635d
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-11-05 08:31:00 +0500

    delete node

--- Data Stucture/28. Linked List.js
+++ Data Stucture/28. Linked List.js
@@ -29,16 +29,37 @@ class List {
         }
     }
 
-    updateNode(node, value) {  // ---> Update Node
-        let counter = 0;
-        let currentNode = this.head;
-        while (counter < this.size) {
-            if (currentNode.value === node) {
-                currentNode.value = value;
-                console.log(`Node ${node} updated to ${value} :`,list);
+    // updateNode(node, value) {  // ---> Update Node
+    //     let counter = 0;
+    //     let currentNode = this.head;
+    //     while (counter < this.size) {
+    //         if (currentNode.value === node) {
+    //             currentNode.value = value;
+    //             console.log(`Node ${node} updated to ${value} :`,list);
+    //         }
+    //         currentNode = currentNode.next;
+    //         counter++;
+    //     }
+    // }
+
+    deleteNode(index) {
+        let counter = 1;
+        let leadNode = this.head;   // ---> leadNode means head node sa phela wali node
+        // console.log("First Time Head Node is:", this.head);
+        // console.log("First Time Lead Node is:", leadNode);
+        if (index === 1) {  // ---> If index 1 hoga tu head index 2 hojai ga r leadNode index 1
+            this.head = this.head.next;
+            // console.log("If index equal to 1 Head Node is:", this.head);
+            // console.log("If index equal to 1 Lead Node is:", leadNode);
+        }
+        else {
+            while (counter < index - 1) {
+                leadNode = leadNode.next;
+                counter++;
             }
-            currentNode = currentNode.next;
-            counter++;
+            let nextNode = leadNode.next.next;
+            leadNode.next = nextNode;
+            console.log(leadNode)
         }
     }
 }
@@ -52,6 +73,8 @@ list.appendNode(500);
 
 // list.updateNode(300, 1000);
 
-list.traversing();
+list.deleteNode(4);
+
+// list.traversing();
 
 console.log(list);
