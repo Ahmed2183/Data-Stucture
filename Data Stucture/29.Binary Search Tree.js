7b982edaab7b4b1142949e13ab6e53f009a91385
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-11-11 15:52:03 +0500

    insert Node

--- Data Stucture/29.Binary Search Tree.js
+++ Data Stucture/29.Binary Search Tree.js
@@ -3,21 +3,63 @@
 
 class Node {
     constructor(val) {
-        this.value = val;
-        this.left = null;
-        this.right = null;
+        this.value = val;    // --> Set root value of tree
+        this.left = null;   // --> In tree first time left is empty/null
+        this.right = null;  // --> In tree first time right is empty/null
     }
 }
 
 class BinaryTree {
     constructor() {
-        this.root = null;
+        this.root = null;  // --> In tree first time root is empty/null
     }
 
     isTreeEmpty() {
         return this.root === null;
     }
+
+    makeTree(val) {
+        let newNode = new Node(val);  // --> Instance/Object of Node Class
+        if (this.root === null) {
+            this.root = newNode;
+        }
+        else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(root, newNode) {
+        // console.log(root.value)
+        // console.log(newNode.value)
+
+        if (root.value > newNode.value) {
+            if (root.left === null) {
+                root.left = newNode;
+            }
+            else {
+                this.insertNode(root.left, newNode);  // --> Recursion of insertNode function
+            }
+        }
+        else {
+            if (root.right === null) {
+                root.right = newNode;
+            }
+            else {
+                this.insertNode(root.right, newNode);
+            }
+        }
+    }
+
 }
 
 let tree = new BinaryTree();
-console.log(tree.isTreeEmpty());
+
+tree.makeTree(20);
+tree.makeTree(10);
+tree.makeTree(5);
+tree.makeTree(30);
+tree.makeTree(40);
+tree.makeTree(23);
+
+// console.log(tree.isTreeEmpty());
+console.log(tree);
